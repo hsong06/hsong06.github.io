2a7a7067c7d0c02797f8fa85143ce9dc894bd4a7
Haohong Song <128619953+hsong06@users.noreply.github.com>
2024-10-28 13:40:38 -0500

    Add files via upload

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,14 +5,29 @@ var addressPoints = [
     -86.7742984
   ],
   [
+    "Galveston,Texas",
+    29.3056996,
+    -94.7933252
+  ],
+  [
     "Oak Ridge, TN",
     36.0103562,
     -84.2696449
   ],
   [
+    "Atlanta,Georgia",
+    33.7489924,
+    -84.3902644
+  ],
+  [
     "Tuscaloosa, Alabama",
     33.2095614,
     -87.5675258
+  ],
+  [
+    "Pacific Grove, California",
+    36.6211149,
+    -121.917794
   ],
   [
     "Philadelphia, Pennsylvania",
